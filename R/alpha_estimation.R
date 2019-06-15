3bddbdfbd75a8c3da12a015528e9b746198bc5a1
Yipeng Song <yipeng.song@hotmail.com>
2019-06-15 15:34:09 +0200

    format R code

--- R/alpha_estimation.R
+++ R/alpha_estimation.R
@@ -27,75 +27,63 @@
 #' \dontrun{alpha_estimation(X,K=3,Rs = 1:15,opts=list())}
 #'
 #' @export
-alpha_estimation <- function(X,K=3,Rs = 1:15, opts=list()){
-  # check if the whole row or whole column is missing
-  # index out the rows and columns not fully missing
-  W <- 1- is.na(X)
-  X <- X[rowSums(W)>0, colSums(W)>0]
-
-  # first center the data sets
-  m <- dim(X)[1]; n <- dim(X)[2]
-  X <- scale(X, center = TRUE, scale = FALSE) # NAs are omitted
-
-  # parameters
-  W <- 1-is.na(X)
-  mn_nonNaN <- sum(W)
-
-  # model selection
-  ratio_mis = 0.1
-  cvErrors <- svd_CV(X,K,Rs,ratio_mis,opts)
-  cvErrors <- cvErrors[,-(K+1)]
- 
-  # select the number of components
-  R_CV <- Rs[apply(cvErrors, 2, which.min)]
-
-  # estimate the noise level
-  alphas_CV <- rep(NA, length(R_CV))
-
-  # first do a SVD on X to accelerate computation
-  R_CV_max <- max(R_CV)
-  if(all(!is.na(X))){
-    svd_tmp <- RSpectra::svds(X,R_CV_max,nu=R_CV_max,nv=R_CV_max)
-    U <- svd_tmp$u
-    S <- diag(svd_tmp$d)
-    V <- svd_tmp$v
-  }else{
-    svd_tmp <- svd_mis(X, R_CV_max, opts)
-    U <- svd_tmp$U
-    S <- diag(as.vector(svd_tmp$S))
-    V <- svd_tmp$V
-  }
-
-  for(i in 1:length(R_CV)){
-    R_CV_tmp <- R_CV[i]
-    Z_hat <- U[,1:R_CV_tmp] %*% S[1:R_CV_tmp,1:R_CV_tmp] %*% t(V[,1:R_CV_tmp])
-    DF <- mn_nonNaN - (m + n)*R_CV_tmp
-    X_nonNaN <- X
-    X_nonNaN[is.na(X)] <- 0
-    Z_hat[is.na(X)] <- 0
-    sigmaSqure <- (1/DF)*norm(X_nonNaN-Z_hat,'F')^2
-    alphas_CV[i] <- sigmaSqure
-  }
-  alphas_mean <- mean(alphas_CV)
-  alphas_std <- stats::sd(alphas_CV)
-
-  result <- list()
-  result$alphas_mean <- alphas_mean
-  result$alphas_std <- alphas_std
-  result$alphas_CV <- alphas_CV
-  result$R_CV <- R_CV
-  result$cvErrors <- cvErrors
-  return(result)
+alpha_estimation <- function(X, K = 3, Rs = 1:15, opts = list()) {
+    # check if the whole row or whole column is missing index out the rows and columns not fully missing
+    W <- 1 - is.na(X)
+    X <- X[rowSums(W) > 0, colSums(W) > 0]
+    
+    # first center the data sets
+    m <- dim(X)[1]
+    n <- dim(X)[2]
+    X <- scale(X, center = TRUE, scale = FALSE)  # NAs are omitted
+    
+    # parameters
+    W <- 1 - is.na(X)
+    mn_nonNaN <- sum(W)
+    
+    # model selection
+    ratio_mis = 0.1
+    cvErrors <- svd_CV(X, K, Rs, ratio_mis, opts)
+    cvErrors <- cvErrors[, -(K + 1)]
+    
+    # select the number of components
+    R_CV <- Rs[apply(cvErrors, 2, which.min)]
+    
+    # estimate the noise level
+    alphas_CV <- rep(NA, length(R_CV))
+    
+    # first do a SVD on X to accelerate computation
+    R_CV_max <- max(R_CV)
+    if (all(!is.na(X))) {
+        svd_tmp <- RSpectra::svds(X, R_CV_max, nu = R_CV_max, nv = R_CV_max)
+        U <- svd_tmp$u
+        S <- diag(svd_tmp$d)
+        V <- svd_tmp$v
+    } else {
+        svd_tmp <- svd_mis(X, R_CV_max, opts)
+        U <- svd_tmp$U
+        S <- diag(as.vector(svd_tmp$S))
+        V <- svd_tmp$V
+    }
+    
+    for (i in 1:length(R_CV)) {
+        R_CV_tmp <- R_CV[i]
+        Z_hat <- U[, 1:R_CV_tmp] %*% S[1:R_CV_tmp, 1:R_CV_tmp] %*% t(V[, 1:R_CV_tmp])
+        DF <- mn_nonNaN - (m + n) * R_CV_tmp
+        X_nonNaN <- X
+        X_nonNaN[is.na(X)] <- 0
+        Z_hat[is.na(X)] <- 0
+        sigmaSqure <- (1/DF) * norm(X_nonNaN - Z_hat, "F")^2
+        alphas_CV[i] <- sigmaSqure
+    }
+    alphas_mean <- mean(alphas_CV)
+    alphas_std <- stats::sd(alphas_CV)
+    
+    result <- list()
+    result$alphas_mean <- alphas_mean
+    result$alphas_std <- alphas_std
+    result$alphas_CV <- alphas_CV
+    result$R_CV <- R_CV
+    result$cvErrors <- cvErrors
+    return(result)
 }
-
-
-
-
-
-
-
-
-
-
-
-
